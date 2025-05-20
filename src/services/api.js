9052c95fb4d9420b2d3b3fedb95911c0207bd3ab
Farooq Ahmed <hello.farooq@outlook.com>
2025-05-21 02:17:30 +0500

    Created Movie App with API Key

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "13ea7bb86c14f52f725abbee944e3d35";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
